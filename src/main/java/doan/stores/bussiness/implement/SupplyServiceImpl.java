3da1ce188975d3250c07f01e6083aafc81823309
Doan <doandv@ominext.com>
2020-03-09 17:53:53 +0700

    create service SupplyServiceImpl.java

--- src/main/java/doan/stores/bussiness/implement/SupplyServiceImpl.java
+++ src/main/java/doan/stores/bussiness/implement/SupplyServiceImpl.java
@@ -3,6 +3,8 @@ package doan.stores.bussiness.implement;
 import doan.stores.bussiness.SupplyService;
 import doan.stores.domain.Supply;
 import doan.stores.dto.request.SupplyRequest;
+import doan.stores.persistenct.SupplyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,14 +12,25 @@ import java.util.List;
 @Service
 public class SupplyServiceImpl implements SupplyService {
 
+    @Autowired
+    private SupplyRepository supplyRepository;
+
     @Override
     public List<Supply> findSupplies() {
-        return null;
+        return supplyRepository.findAll();
     }
 
     @Override
     public void saveSupply(SupplyRequest request) {
-
+        Supply supply = new Supply();
+        supply.setId(request.getId());
+        supply.setName(request.getName());
+        supply.setDescription(request.getDescription());
+        supply.setPhone(request.getPhone());
+        supply.setEmail(request.getEmail());
+        supply.setAddress(request.getAddress());
+        supply.setActive(request.getActive());
+        supplyRepository.save(supply);
     }
 
     @Override
---
+++ src/main/java/doan/stores/persistenct/SupplyRepository.java
@@ -0,0 +1,9 @@
+package doan.stores.persistenct;
+
+import doan.stores.domain.Supply;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplyRepository extends JpaRepository<Supply, Long> {
+}
